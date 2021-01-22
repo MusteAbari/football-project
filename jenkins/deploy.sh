f2973668956f4fe3b7c4fd93dde8f010142ed19e
MusteAbari <m.abari@hotmail.co.uk>
2021-01-22 19:30:13 +0000

    Ansible key

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-scp -i ~/.ssh/football docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
-ssh -i ~/.ssh/football jenkins@swarm-master << EOF
+scp -i ~/.ssh/football_key docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
+ssh -i ~/.ssh/football_key jenkins@swarm-master << EOF
     export DATABASE_URI=${DATABASE_URI} app_version=${app_version} replicas=${replicas}
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml football
 EOF

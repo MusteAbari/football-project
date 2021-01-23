6822d6c7ba97a56fc711582638387e73239eed0b
MusteAbari <m.abari@hotmail.co.uk>
2021-01-23 14:07:45 +0000

    v2

--- team/application/routes.py
+++ team/application/routes.py
@@ -2,27 +2,9 @@ from application import app
 from flask import request, Response
 import random
 
-
-@app.route("/team", methods=["GET"])
-def get_team():
-    teams = ["Arsenal", "Chelsea", "Fulham"]
-    return Response(str(random.choice(teams)), mimetype='text/plain')
-
-
-
-
-
-"""
 @app.route("/team", methods=["GET"])
 def get_team():
     teams = ["Arsenal", "Chelsea", "Fulham", "Liverpool"]
     return Response(str(random.choice(teams)), mimetype='text/plain')
 
-@app.route("/teamposition", methods=["GET"])
-def get_teamposition():
-    team = request.data2.decode("utf-8")
-    position = {"Chelsea" : 1,
-        "Arsenal" : 2,
-        "Fulham" : 3}
-    return jsonify(position[team], mimetype='text/plain')
-"""
+
